7d3c84eb7d18ed13d33591be312360b15293ca89
Edd <60335944+Eddamame@users.noreply.github.com>
2025-12-05 12:54:50 +0800

    add chat response to cohere

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import cohere
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class chatRequest(BaseModel):
     prompt: str
@@ -9,11 +14,23 @@ class chatResponse(BaseModel):
 
 
 app = FastAPI()
+co = cohere.ClientV2(api_key=os.getenv("API_KEY"))
 
 @app.get("/")
 def health():
     return {"status": "Ok! This is working woohoo!"}
 
+
 @app.post("/chat", response_model=chatResponse)
 def chat(request: chatRequest):
-     return chatResponse(response=f"I will get smarter later!")
+    
+    user_prompt = request.prompt
+    
+    response = co.chat(
+        model="command-a-03-2025",
+        messages=[{"role": "user", "content": user_prompt}],
+    )
+
+    final_response = response.message.content[0].text
+
+    return chatResponse(response=f"Cohere said this: {final_response}")
